7d06c4bdbd624994f6493bce79d0960cf1dbc8a5
Jhuliana <jholeanasantos@gmail.com>
2022-08-10 19:46:33 -0300

    fix: little pontuation changes

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -3,15 +3,7 @@ import PostRepository from '../repository/PostRepository.js'
 export async function ShowPosts(req, res) {
 
     try {
-
-        console.log('entrei')
-
-        // const id = 
-
-        const { urlPreview, description } = req.body
-
         const { rows: allPosts } = await PostRepository.getAllPosts();
-
         return res.status(201).send(allPosts)
     }
     catch {
