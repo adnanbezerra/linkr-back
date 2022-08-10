5b11b4316e86c7feafeba503b9d9414316a51e6e
MattGueler <mateus.gueler@gmail.com>
2022-08-10 12:52:49 -0300

    Retorna todos os posts em ordem atualizada

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -2,22 +2,21 @@ import PostRepository from '../repository/PostRepository.js'
 
 export async function ShowPosts(req, res) {
 
-    console.log('entrei')
-    return res.send(200)
+    try {
 
-    // try {
+        console.log('entrei')
 
-    //     // const id = 
+        // const id = 
 
-    //     const { urlPreview, description } = req.body
+        const { urlPreview, description } = req.body
 
-    //     const { rows: allPosts } = await PostRepository.getAllPosts();
+        const { rows: allPosts } = await PostRepository.getAllPosts();
 
-    //     return res.status(201).send(allPosts)
-    // }
-    // catch {
-    //     console.log('cheguei')
-    //     return res.send(500)
-    // }
+        return res.status(201).send(allPosts)
+    }
+    catch {
+        console.log('cheguei')
+        return res.send(500)
+    }
 
 }
--- src/repository/PostRepository.js
+++ src/repository/PostRepository.js
@@ -3,9 +3,9 @@ import connection from "../database/database.js";
 
 async function getAllPosts() {
     return connection.query(`
-      SELECT * 
-      FROM posts
-      ORDER BY "createdAt`)
+        SELECT * 
+        FROM posts
+        ORDER BY posts."createdAt" DESC`)
 }
 
 const PostRepository = {
