9d8963aa86a190fcf252f8f17d2552e8d4bbf1ac
MattGueler <mateus.gueler@gmail.com>
2022-08-10 15:00:17 -0300

    cria o post do usuario e passa pelo validate

--- src/routers/PostRouter.js
+++ src/routers/PostRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { CreatePost, ShowPosts } from "../controllers/PostController.js";
+import { validateSchema } from "../middlewares/ValidateSchema.js";
+import urlSchema from "../schemas/PostSchema.js";
 
 const postRouter = Router();
 
 postRouter.get('/timeline', ShowPosts)
-postRouter.post('/timeline', CreatePost)
+postRouter.post('/timeline', validateSchema(urlSchema), CreatePost)
 
 export default postRouter;
 
