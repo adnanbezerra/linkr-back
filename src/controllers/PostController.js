fc6bea9be270130013fa00d59ad4772417a208a6
MattGueler <mateus.gueler@gmail.com>
2022-08-15 13:45:19 -0300

    teste

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -84,12 +84,12 @@ export async function CreatePost(req, res) {
 
                     const { rows: hashPostExist } = await connection.query(`
                     SELECT * FROM hashtags_posts
-                    WHERE hashtags_posts."postId"=$1 AND hashtags_posts."hashtagId"=$2)`, [bodyHash.idPost, bodyHash.idHash])
+                    WHERE hashtags_posts."postId"=$1 AND hashtags_posts."hashtagId"=$2`, [bodyHash.idPost, bodyHash.idHash])
 
-                    // if (hashPostExist.length === 0) {
-                    //     await connection.query(`
-                    //     INSERT INTO hashtags_posts ("postId","hashtagId") VALUES ($1,$2)`, [bodyHash.idPost, bodyHash.idHash])
-                    // }
+                    if (hashPostExist.length === 0) {
+                        await connection.query(`
+                        INSERT INTO hashtags_posts ("postId","hashtagId") VALUES ($1,$2)`, [bodyHash.idPost, bodyHash.idHash])
+                    }
                 }
 
                 return res.status(201).send(bodyHash)
