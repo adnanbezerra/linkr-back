22dae51cfa74a791930a08af73c0130e9c556975
MattGueler <mateus.gueler@gmail.com>
2022-08-15 12:25:09 -0300

    teste

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -55,30 +55,30 @@ export async function CreatePost(req, res) {
 
                 let hashId;
 
-                for (let counter = 0; counter < arrayHashs.length; counter++) {
-                    let { rows: hashExist } = await connection.query(
-                        `SELECT * FROM hashtags h
-                        WHERE h.name=$1`, [arrayHashs[counter]]
-                    )
+                // for (let counter = 0; counter < arrayHashs.length; counter++) {
+                //     let { rows: hashExist } = await connection.query(
+                //         `SELECT * FROM hashtags h
+                //         WHERE h.name=$1`, [arrayHashs[counter]]
+                //     )
 
-                    hashId = hashExist[0].id
+                //     hashId = hashExist[0].id
 
-                    if (hashExist.length === 0) {
-                        await connection.query(`
-                        INSERT INTO hashtags (name) VALUES ($1)`, [arrayHashs[counter]])
+                //     if (hashExist.length === 0) {
+                //         await connection.query(`
+                //         INSERT INTO hashtags (name) VALUES ($1)`, [arrayHashs[counter]])
 
-                        let { rows: hashExist } = await connection.query(
-                            `SELECT * FROM hashtags h
-                            WHERE h.name=$1`, [arrayHashs[counter]]
-                        )
+                //         let { rows: hashExist } = await connection.query(
+                //             `SELECT * FROM hashtags h
+                //             WHERE h.name=$1`, [arrayHashs[counter]]
+                //         )
 
-                        hashId = hashExist[0].id
-                    }
+                //         hashId = hashExist[0].id
+                //     }
 
-                    // await connection.query(`
-                    // SELECT * FROM hashtags_posts h
-                    // WHERE h."postId"=$1 AND h."hashtagId"=$2)`, [postId, hashId])
-                }
+                //     // await connection.query(`
+                //     // SELECT * FROM hashtags_posts h
+                //     // WHERE h."postId"=$1 AND h."hashtagId"=$2)`, [postId, hashId])
+                // }
 
                 return res.status(201).send(postId)
             }
