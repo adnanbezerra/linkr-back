ca85dfb3d6fa3b3ab3a2d92d50ef54007a4277b6
MattGueler <mateus.gueler@gmail.com>
2022-08-15 15:10:35 -0300

    teste heroku repository

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -79,14 +79,14 @@ export async function CreatePost(req, res) {
                     // SELECT * FROM hashtags_posts
                     // WHERE hashtags_posts."postId"=$1 AND hashtags_posts."hashtagId"=$2`, [bodyHash.idPost, bodyHash.idHash])
 
-                    // if (hashPostExist.length === 0) {
-                    //     await PostRepository.insertPostWithHash(bodyHash.idPost, bodyHash.idHash)
-                    // }
-
                     if (hashPostExist.length === 0) {
-                        await connection.query(`
-                        INSERT INTO hashtags_posts ("postId","hashtagId") VALUES ($1,$2)`, [bodyHash.idPost, bodyHash.idHash])
+                        await PostRepository.insertPostWithHash(bodyHash.idPost, bodyHash.idHash)
                     }
+
+                    // if (hashPostExist.length === 0) {
+                    //     await connection.query(`
+                    //     INSERT INTO hashtags_posts ("postId","hashtagId") VALUES ($1,$2)`, [bodyHash.idPost, bodyHash.idHash])
+                    // }
                 }
 
                 return res.status(201).send(body)
