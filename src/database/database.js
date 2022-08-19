a2462b55bded10455cb19420db4cf9c87f294359
Adnan <adnanbezerra@gmail.com>
2022-08-19 19:26:32 -0300

    prepare for deploy

--- src/database/database.js
+++ src/database/database.js
@@ -4,32 +4,32 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const user = 'postgres';
-const password = process.env.POSTGRES_PASSWORD;
-const host = 'localhost';
-const port = 5432;
-const database = 'Linkr';
+// const user = 'postgres';
+// const password = process.env.POSTGRES_PASSWORD;
+// const host = 'localhost';
+// const port = 5432;
+// const database = 'Linkr';
 
-const connection = new Pool({
-  user,
-  password,
-  host,
-  port,
-  database
-});
+// const connection = new Pool({
+//   user,
+//   password,
+//   host,
+//   port,
+//   database
+// });
 
 
 
 // DB HEROKU
 
 
-// const databaseConfig = {
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// }
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+}
 
-// const connection = new Pool(databaseConfig);
+const connection = new Pool(databaseConfig);
 
 export default connection;
